e4293318b1e87f5303c7021e09697d21c06a6a6f
s1f10240012 <s1f102400123@iniad.org>
2025-07-09 12:14:16 +0900

    詳細ページのテストを追加1

--- todo/tests.py
+++ todo/tests.py
@@ -111,4 +111,4 @@ class TodoViewTestCase(TestCase):
         client = Client()
         response = client.get('/1/')
 
-        self.assertEqual(respoonse.status_code, 404)
+        self.assertEqual(response.status_code, 404)
